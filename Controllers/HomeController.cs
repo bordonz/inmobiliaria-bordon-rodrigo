925b402ebdac1d380678c5f944cb6c8c1e3ae43c
bordonz <rodrigozbordon@gmail.com>
2026-09-15 17:18:51 -0300

    Feat: Configuracion de autenticacion, autorizacion y agregado de vista para acceso restringido

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,4 +21,11 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    public IActionResult Restringido()
+    {
+        ViewData["Title"] = "Acceso denegado";
+        return View();
+    }
+
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,48 @@
+using System.Text;
 using inmobiliaria_airbnb.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "Cookies";
+    options.DefaultChallengeScheme = "Cookies";
+})
+.AddCookie("Cookies", options =>
+{
+    options.LoginPath = "/Usuarios/Login";
+    options.LogoutPath = "/Usuarios/Logout";
+    options.AccessDeniedPath = "/Home/Restringido";
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+    options.SlidingExpiration = true;
+})
+
+.AddJwtBearer("JwtBearer", options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["TokenAuthentication:Issuer"],
+        ValidAudience = builder.Configuration["TokenAuthentication:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["TokenAuthentication:SecretKey"]))
+    };
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Empleado", policy => policy.RequireRole("Empleado", "Administrador"));
+    options.AddPolicy("Administrador", policy => policy.RequireRole("Administrador"));
+});
+
+    
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -11,7 +53,7 @@ builder.Services.AddScoped<IRepositorioReserva, RepositorioReserva>();
 builder.Services.AddScoped<IRepositorioImagen, RepositorioImagen>();
 builder.Services.AddScoped<IRepositorioPago, RepositorioPago>();
 builder.Services.AddScoped<IRepositorioTiposInmuebles, RepositorioTipoInmueble>();
-
+builder.Services.AddScoped<IRepositorioUsuario, RepositorioUsuario>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,6 +67,7 @@ if (!app.Environment.IsDevelopment())
 //app.UseHttpsRedirection();
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
